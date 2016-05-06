74dd849e577477e1df921d06be3b1fccbcce615f
Justin Reynard <framerate@users.noreply.github.com>
2016-05-06 11:23:43 -0700

    Node 6

--- setupEC2.sh
+++ setupEC2.sh
@@ -13,8 +13,8 @@ sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
-# Node 5.X
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+# Node 6.X
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 # update
 sudo apt-get update
