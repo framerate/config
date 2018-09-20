88efaa79e23d1fe038f049349315e79bd82b0cc5
Justin Reynard <framerate@users.noreply.github.com>
2018-09-20 14:22:03 -0700

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,32 +1,42 @@
-#!/bin/bash
+
 if [ "$1" = "" ]
 then
-  echo "Usage: $0 Must pass EMAIL_ADDRESS as first argument!"
-  exit
-fi
+          echo "Usage: $0 Must pass EMAIL_ADDRESS as first argument!"
+            exit
+    fi
 
-echo '>> SETTING USER PW <<'
-#sudo passwd ubuntu
+    echo '>> SETTING USER PW <<'
+    #sudo passwd ubuntu
 
-echo '>> SETTING USER SSH KEY <<'
-ssh-keygen -t rsa -b 4096 -C $1
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+    echo '>> SETTING USER SSH KEY <<'
+    ssh-keygen -t rsa -b 4096 -C $1
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_rsa
 
-# set the local time
-sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+    # set the local time
+    sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
-# Node 8.X
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt-get update
 
-# update
-sudo apt-get update
+    sudo apt-get upgrade -y
 
-# install everything
-sudo apt-get -y --force-yes install git gcc make build-essential python-setuptools nodejs zsh unzip wget software-properties-common mongodb-org
+    sudo apt-get install -y --force-yes gnupg software-properties-common wget
 
-# finaly, print the SSH key so we can add it to github...
-cat ~/.ssh/id_rsa.pub
+    # Node 8.X
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
-# install zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # Atom
+    wget https://github.com/atom/atom/releases/download/v1.9.2/atom-amd64.deb
+    sudo dpkg -i atom-amd64.deb
+
+    # update
+    sudo apt-get update
+
+    # install everything
+    sudo apt-get -y --force-yes install git gcc g++ make build-essential python-setuptools nodejs zsh unzip wget software-properties-common mongodb atom
+
+    # finaly, print the SSH key so we can add it to github...
+    cat ~/.ssh/id_rsa.pub
+
+    # install zsh
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
