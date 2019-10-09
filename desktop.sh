708b197e1653707e95b8e944938f5894838755b3
Justin Reynard <framerate@users.noreply.github.com>
2019-10-08 17:55:01 -0700

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,54 +1,54 @@
+#!/bin/bash
+
 if [ "$1" = "" ]
 then
-          echo "Usage: $0 Must pass EMAIL_ADDRESS as first argument!"
-            exit
-    fi
+  echo ":: Usage: $0 Must pass EMAIL_ADDRESS as first argument!"
+  exit
+fi
 
-    echo '>> SETTING USER PW <<'
-    sudo passwd $USER
+echo ':: SETTING USER PW'
+sudo passwd $USER
 
-    echo '>> SETTING USER SSH KEY <<'
-    ssh-keygen -t rsa -b 4096 -C $1
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_rsa
+echo ':: SETTING USER SSH KEY'
+ssh-keygen -t rsa -b 4096 -C $1
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
 
-    # set the local time
-    sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+# set the local time
+echo ':: SETTING LOCAL TIME'
+sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
-    sudo apt-get update
+# Node 10.X
+echo ':: Adding Repository for NodeJS'
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
-    sudo apt-get upgrade -y
+# Update Aptitude
+echo ':: Updating Sources'
+sudo apt-get update
+sudo apt-get upgrade -y
 
-    sudo apt-get install -y --force-yes gnupg software-properties-common wget
+# Install Foundation
+echo ':: Installing System Foundation...'
+sudo apt-get -y --force-yes install gnupg software-properties-common wget git gcc g++ make build-essential python-setuptools zsh unzip wget software-properties-common
 
-    # Node 8.X
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+# Atom
+echo ':: Downloading & Installing Deb for Atom'
+wget "https://atom.io/download/deb" -O atom-amd64.deb
+sudo dpkg -i atom-amd64.deb
 
-    # install everything
-    echo "*** INSTALLING FOUNDATION ***"
-    sudo apt-get -y --force-yes install git gcc g++ make build-essential python-setuptools zsh unzip wget software-properties-common
+echo ':: Fixing Broken Installs'
+sudo apt-get -y --fix-broken install
 
-    # Atom
-    wget "https://atom.io/download/deb" -O atom-amd64.deb
-    sudo dpkg -i atom-amd64.deb
+# install troublesome ones
+echo ":: INSTALLING TROUBLESOME STUFF"
+sudo apt-get -y install mongodb nodejs
 
-    # update
-    sudo apt-get update
-    
-    # install troublesome ones
-    echo "*** INSTALLING TROUBLESOME STUFF ***"
-    sudo apt-get -y install mongodb nodejs atom
+# install nvm
+echo ":: Installing Node Version Manager"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 
-    # fix it
-    echo "*** FIXING BROKEN INSTALLS ***"    
-    sudo apt-get -y --fix-broken install
-    
-    # install nvm
-    echo "*** INSTALL NVM ***"    
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+echo ":: Installing OhMyZsh"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-    # finaly, print the SSH key so we can add it to github...
-    cat ~/.ssh/id_rsa.pub
-
-    # install zsh
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# finaly, print the SSH key so we can add it to github...
+cat ~/.ssh/id_rsa.pub
